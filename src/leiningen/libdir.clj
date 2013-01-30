eedd14370759c6f65afda25cf4911458fa9555ee
Moritz Heidkamp <moritz@doo.net>
2013-01-30 11:32:35 +0100

    Add filter option
    This patch adds an option to filter the dependencies to be copied into
    the libdir. Possible values are a regular expression which is matched on
    the dependency jar file names or a predicate which is applied to the
    java.io.File object to check for whether it is to be copied.
    
    The config structure is changed so that all libdir specific options now
    reside below the :libdir key in the project defintion. :libdir-path is
    kept for backwards compatibility, though.

--- src/leiningen/libdir.clj
+++ src/leiningen/libdir.clj
@@ -15,22 +15,39 @@
       (io/copy file target-file))))
 
 (defn copy-deps
-  [project target-dir sourceify?]
+  [project target-dir filter-pred sourceify?]
   (let [project (project/unmerge-profiles project [:default])
         deps (->> (classpath/resolve-dependencies :dependencies project)
-                  (filter #(and (.endsWith (.getName %) ".jar") (.exists %))))]
+                  (filter #(and (.endsWith (.getName %) ".jar") (.exists %)))
+                  (filter filter-pred))]
     (.mkdirs target-dir)
     (copy-files target-dir deps)
     (main/info "Copied" (count deps) "file(s) to:" (.getAbsolutePath target-dir))))
 
+(defn file-matches [re file]
+  (re-find re (str file)))
+
 (defn libdir
   "Copy jar dependencies into the project lib directory.
 
-Set :libdir-path in the project to a string containing the relative path
-of the target lib dir.  If omitted, the default is \"lib\"."
+The following options can be specified under the :libdir key in the
+project definition:
+
+  :path - A string containing the relative path of the target lib
+          dir. If omitted, the default is \"lib\".
+  :filter - A regular expression which must match on file names of jar
+            file names to be copied or a function which should be a predicate
+            accepting java.io.File objects and returning whether it should be
+            copied or not. The default is to copy all files."
   [project & args]
-  (let [target-dir (if-let [path (:libdir-path project)]
-                     (java.io.File. path)
-                     (java.io.File. (:root project) "lib"))]
-    (copy-deps project target-dir nil)))
+  (let [options (:libdir project)
+        target-dir  (if-let [path (or (:path options) (:libdir-path project))]
+                      (java.io.File. path)
+                      (java.io.File. (:root project) "lib"))
+        filter-pred (let [filter (:filter options)]
+                      (cond (fn? filter) filter
+                            (instance? java.util.regex.Pattern filter)
+                            (partial file-matches filter)
+                            (not filter) identity))]
+    (copy-deps project target-dir filter-pred nil)))
 
